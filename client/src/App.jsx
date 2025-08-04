960f577769bb6755a2b21a45524896c784b447f4
suparshvabarwade03 <suparshva2209@gmail.com>
2025-08-04 19:25:28 +0530

    bgImg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
 
   return (
-    <div className="bg-[url('./bgImage.svg')] bg-contain">
+    <div className="bg-[url('/bgImage.svg')] bg-contain">
       <Toaster />
         <Routes>
           <Route path='/' element={ authUser ? <HomePage /> : <Navigate to="/login" /> } />
