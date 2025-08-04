8397126cf9d2595ed0d6f445f6bc452e21852aac
suparshvabarwade03 <suparshva2209@gmail.com>
2025-08-04 19:14:48 +0530

    edited server file

--- server/server.js
+++ server/server.js
@@ -49,7 +49,10 @@ app.use("/api/messages", messageRouter);
 // connecting to db 
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+} 
 
- 
+// export server for vercel 
+export default server;
